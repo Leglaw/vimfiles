12292fe9f48006d2eac78cdefdf6154500dfbed6
Craig Hess <chess@shamrocktradingcorp.com>
2026-03-05 14:19:19 -0600

    Update treesitter config to work with latest version

--- conf.d/plugins.d/nvim-treesitter.lua
+++ conf.d/plugins.d/nvim-treesitter.lua
@@ -1,22 +1,9 @@
-require'nvim-treesitter.configs'.setup {
-  ensure_installed = {
-    "c", "lua", "vim", "vimdoc", "query" 
-  },
-  sync_install = false,
-  auto_install = true,
-  -- ignore_install = {},
-
-  -- Treesitter module config
-  highlight = { enable = true },
-  incremental_selection = {
-    enable = true,
-    keymaps = {
-      init_selection = "gnn",
-      node_incremental = "grn",
-      scope_incremental = "grc",
-      node_decremental = "grm",
-    },
-  },
-  indent = { enable = true }, -- Experimental
+require'nvim-treesitter'.install {
+  'c', 'lua', 'vim', 'vimdoc', 'query',
+  'rust', 'javascript', 'zig', 'typescript'
 }
 
+vim.api.nvim_create_autocmd('FileType', {
+  pattern = { '<filetype>' },
+  callback = function() vim.treesitter.start() end,
+})
